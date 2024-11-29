95d7c46de165c0cfb85a2f556a7cb8c9f00593b3
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-11-29 19:45:44 -0300

    add server config

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,29 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common'
+import config from './config'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(3000)
+
+  //
+  // Validations
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      // true en development, false en production:
+      enableDebugMessages: config.NODE_ENV === 'development',
+      // false en development, true en production:
+      disableErrorMessages: config.NODE_ENV === 'production',
+    }),
+  )
+
+  //
+  // Listen
+
+  await app.listen(config.PORT)
 }
 bootstrap()

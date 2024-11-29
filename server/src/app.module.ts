78aa4c61c9abc4c52283372c7c9811dd0ad54460
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-11-29 20:07:33 -0300

    add swagger & auth0

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
+import { AuthzModule } from './authz/authz.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthzModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}

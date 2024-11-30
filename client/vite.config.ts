478996bc0b921f6162f9e96c3b7927cc4222f6b1
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-11-30 00:48:23 -0300

    add tabler icons

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,4 +10,10 @@ export default defineConfig({
     tsconfigPaths({ projects: ['./tsconfig.app.json'] }),
     react(),
   ],
+  resolve: {
+    alias: {
+      // /esm/icons/index.mjs only exports the icons statically, so no separate chunks are created
+      '@tabler/icons-react': '@tabler/icons-react/dist/esm/icons/index.mjs',
+    },
+  },
 })

4093f78ebf5a8456f6ccf2c403c7aae788fa358e
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-11-29 23:44:34 -0300

    add vite-tsconfig-paths

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite(), react()],
+  plugins: [
+    TanStackRouterVite(),
+    tsconfigPaths({ projects: ['./tsconfig.app.json'] }),
+    react(),
+  ],
 })

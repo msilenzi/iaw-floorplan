0c472d65d011cc3e4cb9a16fde4283f1c899bcfa
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-12-01 10:40:51 -0300

    add Organizations header

--- client/src/routes/_protected/organizations/_layout/index.lazy.tsx
+++ client/src/routes/_protected/organizations/_layout/index.lazy.tsx
@@ -1,9 +1,58 @@
-import { createLazyFileRoute } from '@tanstack/react-router'
+import {
+  Anchor,
+  Breadcrumbs,
+  Button,
+  Container,
+  Group,
+  Menu,
+  Title,
+} from '@mantine/core'
+import { IconBuildingPlus, IconBuildings, IconPlus } from '@tabler/icons-react'
+import { createLazyFileRoute, Link } from '@tanstack/react-router'
 
 export const Route = createLazyFileRoute('/_protected/organizations/_layout/')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello &quot;/_protected/organizations/_layout/&quot;!</div>
+  return (
+    <Container size="md" mt="3rem">
+      <Breadcrumbs
+        pb="md"
+        styles={{
+          separator: { opacity: 0.5 },
+        }}
+        fz="sm"
+      >
+        <Anchor c="dimmed" fz="sm" component={Link} disabled>
+          Organizaciones
+        </Anchor>
+      </Breadcrumbs>
+      <Group justify="space-between">
+        <Title order={1}>Organizaciones</Title>
+        <Menu position="bottom-end" shadow="md">
+          <Menu.Target>
+            <Button
+              rightSection={<IconPlus size={16} stroke={3} />}
+              size="sm"
+              variant="gradient"
+              gradient={{ from: 'blue', to: 'cyan', deg: 90 }}
+            >
+              Agregar
+            </Button>
+          </Menu.Target>
+          <Menu.Dropdown>
+            <Menu.Item
+              leftSection={<IconBuildingPlus size={14} stroke={2.5} />}
+            >
+              Crear organización
+            </Menu.Item>
+            <Menu.Item leftSection={<IconBuildings size={14} stroke={2.5} />}>
+              Unirse a una organización
+            </Menu.Item>
+          </Menu.Dropdown>
+        </Menu>
+      </Group>
+    </Container>
+  )
 }

c27ab3860b08b3301768c4ae6cfac58e66370fe4
Manuel Silenzi <manuelsilenzi1@gmail.com>
2024-12-26 16:38:08 -0300

    generate organizations module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose'
 
 import cfg from './cfg'
 import { AuthModule } from './modules/auth/auth.module'
+import { OrganizationsModule } from './modules/organizations/organizations.module'
 
 @Module({
-  imports: [MongooseModule.forRoot(cfg.MONGODB_URI), AuthModule],
+  imports: [
+    MongooseModule.forRoot(cfg.MONGODB_URI),
+    AuthModule,
+    OrganizationsModule,
+  ],
   controllers: [],
   providers: [],
 })

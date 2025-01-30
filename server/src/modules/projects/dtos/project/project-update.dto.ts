48ad97bd32b3f7c29957d9fe3e5fc872b4f3c11e
Manuel Silenzi <manuelsilenzi1@gmail.com>
2025-01-30 18:53:55 -0300

    add projects/dtos subfolders

--- server/src/modules/projects/dtos/project/project-update.dto.ts
+++ server/src/modules/projects/dtos/project/project-update.dto.ts
@@ -10,41 +10,44 @@ import {
 } from 'class-validator'
 
 import { IsTrimmedString } from 'src/common/decorators'
-import { ProjectPurpose } from '../types/project-purpose.enum'
-import { ProjectStatus } from '../types/project-status.enum'
-import { ProjectType } from '../types/project-type.enum'
-import { ProjectOwnerDto } from './project-owner.dto'
-import { ProjectProfessionalDto } from './project-professional.dto'
-import { ProjectRequirementDto } from './project-requirement.dto'
+import { ProjectPurpose } from '../../types/project-purpose.enum'
+import { ProjectStatus } from '../../types/project-status.enum'
+import { ProjectType } from '../../types/project-type.enum'
+import { ProjectOwnerDto } from '../project/project-owner.dto'
+import { ProjectProfessionalDto } from '../project/project-professional.dto'
+import { ProjectRequirementDto } from '../project/project-requirement.dto'
 
-export class ProjectCreateDto {
+export class ProjectUpdateDto {
+  @IsOptional()
   @IsTrimmedString({
     isEmptyMessage: 'El expediente es obligatorio',
     isNotStringMessage: 'Expediente inválido',
   })
-  readonly record: string
+  readonly record?: string
 
   @IsOptional()
   @IsTrimmedString({
     isEmptyMessage: 'El nombre no puede estar vacío',
     isNotStringMessage: 'Nombre inválido',
   })
-  readonly name?: string
+  readonly name?: string | null
 
+  @IsOptional()
   @ApiProperty({ enum: ProjectType, enumName: 'ProjectType' })
   @IsEnum(ProjectType, { message: 'Tipo de proyecto inválido' })
-  readonly type: ProjectType
+  readonly type?: ProjectType
 
+  @IsOptional()
   @ApiProperty({ enum: ProjectPurpose, enumName: 'ProjectPurpose' })
   @IsEnum(ProjectPurpose, { message: 'Destino inválido' })
-  readonly purpose: ProjectPurpose
+  readonly purpose?: ProjectPurpose
 
   @IsOptional()
   @IsTrimmedString({
     isEmptyMessage: 'La ubicación no puede estar vacía',
     isNotStringMessage: 'Ubicación inválida',
   })
-  readonly location?: string
+  readonly location?: string | null
 
   @IsOptional()
   @IsArray({ message: 'Las referencias deben ser un arreglo' })
@@ -54,34 +57,34 @@ export class ProjectCreateDto {
     isNotStringMessage: 'Las referencias deben ser cadenas de texto',
     validationOptions: { each: true },
   })
-  readonly references?: string[]
+  readonly references?: string[] | null
 
   @IsOptional()
   @IsString({ message: 'Antecedentes inválidos' })
-  readonly background?: string
+  readonly background?: string | null
 
   @IsOptional()
   @ValidateNested()
   @Type(() => ProjectOwnerDto)
-  readonly owner?: ProjectOwnerDto
+  readonly owner?: ProjectOwnerDto | null
 
   @IsOptional()
   @ValidateNested({ each: true })
   @Type(() => ProjectProfessionalDto)
-  readonly designers?: ProjectProfessionalDto[]
+  readonly designers?: ProjectProfessionalDto[] | null
 
   @IsOptional()
   @ValidateNested({ each: true })
   @Type(() => ProjectProfessionalDto)
-  readonly technicalDirectors?: ProjectProfessionalDto[]
+  readonly technicalDirectors?: ProjectProfessionalDto[] | null
 
   @IsOptional()
   @ApiProperty({ enum: ProjectStatus, enumName: 'ProjectStatus' })
   @IsEnum(ProjectStatus, { message: 'Tipo de proyecto inválido' })
-  readonly status?: ProjectStatus
+  readonly status?: ProjectStatus | null
 
   @IsOptional()
   @ValidateNested({ each: true })
   @Type(() => ProjectRequirementDto)
-  readonly otherRequirements?: ProjectRequirementDto[]
+  readonly otherRequirements?: ProjectRequirementDto[] | null
 }

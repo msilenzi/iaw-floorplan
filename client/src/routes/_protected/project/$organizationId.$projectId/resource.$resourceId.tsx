895f7184bd9bd886a509ff0b6e8463fa2631af17
Manuel Silenzi <manuelsilenzi1@gmail.com>
2025-01-30 14:19:59 -0300

    validate resource id

--- client/src/routes/_protected/project/$organizationId.$projectId/resource.$resourceId.tsx
+++ client/src/routes/_protected/project/$organizationId.$projectId/resource.$resourceId.tsx
@@ -1,9 +1,13 @@
 import { createFileRoute, useNavigate } from '@tanstack/react-router'
 import { Modal, Stack } from '@mantine/core'
 
+import { BasicCtaBanner } from '@Common/components/BasicCtaBanner'
+import { SectionContainer } from '@Common/components/SectionContainer'
+import { getErrorResponse } from '@Common/utils/errorHandling'
 import { ViewResourceBody } from '@ProjectResources/components/ViewResource/ViewResourceBody'
 import { ViewResourceHeader } from '@ProjectResources/components/ViewResource/ViewResourceHeader'
 import { CurrentResourceProvider } from '@ProjectResources/context/CurrentResource/CurrentResourceProvider'
+import { useProjectResourceQuery } from '@ProjectResources/hooks/useProjectResourceQuery'
 
 export const Route = createFileRoute(
   '/_protected/project/$organizationId/$projectId/resource/$resourceId',
@@ -13,9 +17,13 @@ export const Route = createFileRoute(
 
 function RouteComponent() {
   const { organizationId, projectId, resourceId } = Route.useParams()
+  const resourceQuery = useProjectResourceQuery(projectId, resourceId)
+
   const navigate = useNavigate()
 
-  // TODO: validar resourceId
+  if (resourceQuery.isError) {
+    return <ShowError error={resourceQuery.error} />
+  }
 
   function handleClose() {
     void navigate({
@@ -37,5 +45,34 @@ function RouteComponent() {
         </Modal.Content>
       </Modal.Root>
     </CurrentResourceProvider>
+  )
+}
+
+type ShowErrorProps = {
+  error: Error
+}
+
+function ShowError({ error }: ShowErrorProps) {
+  const { organizationId, projectId } = Route.useParams()
+  const navigate = useNavigate()
+
+  const { title, message } = getErrorResponse(error, {
+    title: 'No pudimos obtener el recurso',
+  })
+
+  return (
+    <SectionContainer>
+      <BasicCtaBanner
+        title={title}
+        description={message}
+        buttonText="Volver a recursos"
+        onClick={() =>
+          void navigate({
+            to: '/project/$organizationId/$projectId/resources',
+            params: { organizationId, projectId },
+          })
+        }
+      />
+    </SectionContainer>
   )
 }
